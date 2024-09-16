9535fcdf0387031ecf9ba4084ba90fb795caaeb4
aleksGdz <aleksgaldamez2002@gmail.com>
2024-09-16 11:50:38 -0600

    class entities

---
+++ src/main/java/org/agaldamez/spring/datajpa/app/models/entity/Cliente.java
@@ -0,0 +1,79 @@
+package org.agaldamez.spring.datajpa.app.models.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date createAt;
+
+    public Cliente () {}
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName () {
+        return firstName;
+    }
+
+    public void setFirstName (String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName () {
+        return lastName;
+    }
+
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail () {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt () {
+        return createAt;
+    }
+
+    public void setCreateAt (Date createAt) {
+        this.createAt = createAt;
+    }
+
+}
